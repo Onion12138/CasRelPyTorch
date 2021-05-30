73e157a528117ba0475b9b222391f81ed9983d69
onion <969023014@qq.com>
2021-05-30 15:37:42 +0800

    modify dataloader for readability

--- model/data.py
+++ model/data.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from transformers import BertTokenizer
 from collections import defaultdict
-
+from torch.nn.utils.rnn import pad_sequence
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 num_rel = 18
 
@@ -92,31 +92,39 @@ class MyDataset(DataSet):
     def __len__(self):
         return len(self.dataset)
 
-
 def my_collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
     batch.sort(key=lambda x: x[2], reverse=True)
     token_ids, masks, text_len, sub_heads, sub_tails, sub_head, sub_tail, obj_heads, obj_tails, triples = zip(*batch)
-    cur_batch = len(batch)
-    max_text_len = max(text_len)
-    batch_token_ids = torch.zeros((cur_batch, max_text_len), dtype=torch.long)
-    batch_masks = torch.zeros((cur_batch, max_text_len), dtype=torch.long)
-    batch_sub_heads = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
-    batch_sub_tails = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
-    batch_sub_head = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
-    batch_sub_tail = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
-    batch_obj_heads = torch.zeros((cur_batch, max_text_len, num_rel), dtype=torch.float)
-    batch_obj_tails = torch.zeros((cur_batch, max_text_len, num_rel), dtype=torch.float)
+    # cur_batch = len(batch)
+    # max_text_len = max(text_len)
 
-    for i in range(cur_batch):
-        batch_token_ids[i, :text_len[i]].copy_(torch.from_numpy(token_ids[i]))
-        batch_masks[i, :text_len[i]].copy_(torch.from_numpy(masks[i]))
-        batch_sub_heads[i, :text_len[i]].copy_(torch.from_numpy(sub_heads[i]))
-        batch_sub_tails[i, :text_len[i]].copy_(torch.from_numpy(sub_tails[i]))
-        batch_sub_head[i, :text_len[i]].copy_(torch.from_numpy(sub_head[i]))
-        batch_sub_tail[i, :text_len[i]].copy_(torch.from_numpy(sub_tail[i]))
-        batch_obj_heads[i, :text_len[i], :].copy_(torch.from_numpy(obj_heads[i]))
-        batch_obj_tails[i, :text_len[i], :].copy_(torch.from_numpy(obj_tails[i]))
+    batch_token_ids = pad_sequence(token_ids, batch_first=True)
+    batch_masks = pad_sequence(masks, batch_first=True)
+    batch_sub_heads = pad_sequence(sub_heads, batch_first=True)
+    batch_sub_tails = pad_sequence(sub_tails, batch_first=True)
+    batch_sub_head = pad_sequence(sub_head, batch_first=True)
+    batch_sub_tail = pad_sequence(sub_tail, batch_first=True)
+    batch_obj_heads = pad_sequence(obj_heads, batch_first=True)
+    batch_obj_tails = pad_sequence(obj_tails, batch_first=True)
+    # batch_token_ids = torch.zeros((cur_batch, max_text_len), dtype=torch.long)
+    # batch_masks = torch.zeros((cur_batch, max_text_len), dtype=torch.long)
+    # batch_sub_heads = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
+    # batch_sub_tails = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
+    # batch_sub_head = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
+    # batch_sub_tail = torch.zeros((cur_batch, max_text_len), dtype=torch.float)
+    # batch_obj_heads = torch.zeros((cur_batch, max_text_len, num_rel), dtype=torch.float)
+    # batch_obj_tails = torch.zeros((cur_batch, max_text_len, num_rel), dtype=torch.float)
+    #
+    # for i in range(cur_batch):
+    #     batch_token_ids[i, :text_len[i]].copy_(torch.from_numpy(token_ids[i]))
+    #     batch_masks[i, :text_len[i]].copy_(torch.from_numpy(masks[i]))
+    #     batch_sub_heads[i, :text_len[i]].copy_(torch.from_numpy(sub_heads[i]))
+    #     batch_sub_tails[i, :text_len[i]].copy_(torch.from_numpy(sub_tails[i]))
+    #     batch_sub_head[i, :text_len[i]].copy_(torch.from_numpy(sub_head[i]))
+    #     batch_sub_tail[i, :text_len[i]].copy_(torch.from_numpy(sub_tail[i]))
+    #     batch_obj_heads[i, :text_len[i], :].copy_(torch.from_numpy(obj_heads[i]))
+    #     batch_obj_tails[i, :text_len[i], :].copy_(torch.from_numpy(obj_tails[i]))
 
     return {
                "token_ids": batch_token_ids.to(device),
